ec5953483bda4e5b825b626604e4c520a7789ee5
Gustavo Santana <gustavo.rsantana@hotmail.com>
2023-05-02 18:33:54 -0300

    exercicio 1

---
+++ fundamentos/secao-3-introducao-JS/dia-1-JS-primeiros-passos/exercicio1.js
@@ -0,0 +1,13 @@
+let n1 = 10;
+let n2 = 15;
+
+let adicao = n1 + n2;
+let subtracao = n1 - n2;
+let multiplicacao = n1 * n2;
+let divisao = n1 / n2;
+
+
+console.log(`a soma entre ${n1} e ${n2} e de ${adicao}`);
+console.log(`a subtração entre ${n1} e ${n2} e de ${subtracao}`);
+console.log(`a multiplicação entre ${n1} e ${n2} e de ${multiplicacao}`);
+console.log(`a divisão entre ${n1} e ${n2} e de ${divisao}`);
